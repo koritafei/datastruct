e13f7cc53cd7875b04692ae6e1f2cfd5592a53a4
koritafei <koritafei@gmail.com>
2021-05-24 18:37:48 +0800

    sort排序扩展

---
+++ sort/KthLarge.cpp
@@ -0,0 +1,67 @@
+/**
+ * @ Description  : kth large number
+ * @ Version      : 1.0
+ * @ Author       : koritafei(koritafei@gmail.com)
+ * @ Date         : 2021-05-23 17:23:49
+ * @ LastEditors  : koritafei(koritafei@gmail.com)
+ * @ LastEditTime : 2021-05-24 18:37:22
+ * @ FilePath     : /datastruct/sort/KthLarge.cpp
+ * @ Copyright (C) 2021 koritafei(koritafei@gmail.com). All rights reserved.
+ * */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+void printVector(std::vector<T> &nums) {
+  for (auto item : nums) {
+    std::cout << item << " ";
+  }
+  std::cout << std::endl;
+}
+
+template <class T>
+int partition(std::vector<T> &nums, int start, int end) {
+  T   key = nums[start];
+  int i = start + 1, j = end;
+  while (i <= j) {
+    while (i <= end && nums[i] <= key) {
+      i++;
+    }
+    while (j >= start && nums[j] > key) {
+      j--;
+    }
+    std::swap(nums[i++], nums[j--]);
+  }
+
+  std::swap(nums[start], nums[j]);
+  return j;
+}
+
+template <class T>
+T findKthLarge(std::vector<T> &nums, int start, int end, int kth) {
+  if (start >= end || kth < 0) {
+    return 0;
+  }
+
+  int p = partition(nums, start, end);
+  if (p == kth - 1) {
+    return nums[p];
+  } else if (p < kth - 1) {
+    return findKthLarge(nums, p + 1, end, kth);
+  } else if (p > kth - 1) {
+    return findKthLarge(nums, start, p - 1, kth);
+  }
+
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  std::vector<int> nums = {1, 10, 8, 2, 6, 1};
+  int              k    = 4;
+  printVector(nums);
+  int num = findKthLarge(nums, 0, nums.size() - 1, k);
+  std::cout << k << '\t' << num << std::endl;
+}
---
+++ sort/pairsNum.cpp
@@ -0,0 +1,69 @@
+/**
+ * @ Description  : 逆序对数
+ * @ Version      : 1.0
+ * @ Author       : koritafei(koritafei@gmail.com)
+ * @ Date         : 2021-05-23 17:23:31
+ * @ LastEditors  : koritafei(koritafei@gmail.com)
+ * @ LastEditTime : 2021-05-24 16:57:36
+ * @ FilePath     : /datastruct/sort/pairsNum.cpp
+ * @ Copyright (C) 2021 koritafei(koritafei@gmail.com). All rights reserved.
+ * */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+void printVector(std::vector<T> &nums) {
+  for (auto item : nums) {
+    std::cout << item << " ";
+  }
+  std::cout << std::endl;
+}
+
+template <class T>
+int mergeSort(std::vector<T> &nums, int start, int end) {
+  if (start >= end) {
+    return 0;
+  }
+
+  int mid   = start + (end - start) / 2;
+  int left  = mergeSort(nums, start, mid);
+  int right = mergeSort(nums, mid + 1, end);
+
+  std::vector<T> copy(nums.size());
+
+  int count = 0;
+  int i     = mid;
+  int j     = end;
+  int k     = end;
+  while (i >= start && j >= mid + 1) {
+    if (nums[i] > nums[j]) {
+      copy[k--] = nums[i--];
+      count += j - mid;
+    } else {
+      copy[k--] = nums[j--];
+    }
+  }
+
+  while (i >= start) {
+    copy[k--] = nums[i--];
+  }
+  while (j >= mid + 1) {
+    copy[k--] = nums[j--];
+  }
+
+  for (int t = start; t <= end; t++) {
+    nums[t] = copy[t];
+  }
+
+  return left + right + count;
+}
+
+int main(int argc, char **argv) {
+  std::vector<int> nums = {6, 1, 3, 5, 2, 10};
+  printVector(nums);
+  std::cout << mergeSort(nums, 0, nums.size() - 1) << std::endl;
+  printVector(nums);
+}
