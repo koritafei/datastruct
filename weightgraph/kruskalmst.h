253765e21965eb1c03b5ed44c8514d09acd124f6
koritafei <koritafei@gmail.com>
2021-12-29 10:32:07 +0800

    数据结构

--- weightgraph/kruskalmst.h
+++ weightgraph/kruskalmst.h
@@ -21,48 +21,6 @@
 template <typename Graph, typename Weight>
 class KrulkalMst {
 public:
-  KrulkalMst(Graph &graph) {
-    std::priority_queue<Edge<Weight>,
-                        std::vector<Edge<Weight>>,
-                        std::greater<Edge<Weight>>>
-        pq;
-    for (int i = 0; i < graph.V(); i++) {
-      typename Graph::adjIterator adj(graph, i);
-      for (Edge<Weight> *e = adj.begin(); !adj.end(); e = adj.next()) {
-        if (e->v() < e->w()) {
-          pq.push(*e);
-        }
-      }
-    }
-
-    Union un(graph.V());
-    while (pq.size() && mst.size() < graph.V() - 1) {
-      Edge<Weight> *e = pq.top();
-      pq.pop();
-      if (un.isConnected(e->v(), e->w())) {
-        continue;
-      }
-      mst.push_back(*e);
-      un.unionElement(e->v(), e->w());
-    }
-
-    mstWeight = mst[0].wt();
-    for (int i = 1; i < mst.size(); i++) {
-      mstWeight += mst[i].wt();
-    }
-  }
-
-  ~KrulkalMst() {
-  }
-
-  std::vector<Edge<Weight>> mstEdges() const {
-    return mst;
-  }
-
-  Weight weight() const {
-    return weight;
-  }
-
 private:
   std::vector<Edge<Weight>> mst;
   Weight                    mstWeight;
